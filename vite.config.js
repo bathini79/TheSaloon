bf834cb0b286c88979fd64a3a993c4f9dfeea1fc
Bathini Nipun <116077457+bathini79@users.noreply.github.com>
2024-11-24 05:30:38 +0000

    feat:added form ,shadcn,appwrite

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
-// https://vitejs.dev/config/
+import path from "path"
+ // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   test: {
     globals: true,
     environment: 'jsdom',
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
 })
